e2962f1acfcd3a0175e8d6a4b1cb851a04ae1975
dedvolodya <vantuyyya@gmail.com>
2017-10-25 19:05:13 +0300

    deleted bad code

--- Lab2/Lab2.c
+++ Lab2/Lab2.c
@@ -16,46 +16,41 @@ int main() {
 		_asm {
 			
 			// case 9 
-				mov	 eax, p
-			        cmp	 p, 9
+			     cmp	 p, 9
 				je	 SHORT case9
 
 			// case 7 
-			         mov	 eax, p
-			         cmp	 p, 7
-			         je	 SHORT case7
+			      cmp	 p, 7
+			      je	 SHORT case7
 
 			// case 4 
-			         mov	 eax, p
-			         cmp	 p, 4
-			         je	 SHORT case4
+				 cmp	 p, 4
+			      je	 SHORT case4
 
 			// case 2 
-				mov	 eax, p
-				cmp	 p, 2
-				je	 SHORT case2
+				 cmp	 p, 2
+				 je	 SHORT case2
 
 			// case 0 
-				mov	 eax, p
-				cmp	 p, 0
-				je	 SHORT case0
+				 cmp	 p, 0
+				 je	 SHORT case0
 
 			// default	
-                                jmp def
+                       jmp def
              
 			// j *= 2; break;
-			        case9 :
+			     case9 :
 				    shl j,1
 				    jmp END
 
-                        // j += 15; break;
+                 // j += 15; break;
 				case7:
-                                    add j,15
-            			    jmp END
+                          add j,15
+            		    jmp END
 
 			 // j = -5; break;
 				case4:
-                                    mov j,-5
+                          mov j,-5
 				    jmp END
 
 			 // j -= 8; break;
@@ -65,7 +60,7 @@ int main() {
 			
 			 // j = 0; break;
 		 		case0 :
-             			    mov j, 0
+             		    mov j, 0
 				    jmp END
                          // j = 77
 				def:
